2e16da03848bd37b2ca40d8031ef96735d8b5239
Manoj Prabhakar <manojprabhakar.isb@gmail.com>
2024-11-16 10:57:03 +0000

    Created Initial Version of translation package

--- tests/test.py
+++ tests/test.py
@@ -15,3 +15,8 @@ def test_translation_hindi():
     translator = Translator()
     result = translator.translate("Hello, how are you?", lang="hi")
     assert result is not None
+
+def test_translation_english():
+    translator = Translator()
+    result = translator.translate("Bomjour", lang="en")
+    assert result is not None
